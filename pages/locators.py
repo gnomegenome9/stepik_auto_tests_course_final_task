6b093656e42523842ed9995a4b9e8cea6c1dd75a
gnomegenome9 <125520515+gnomegenome9@users.noreply.github.com>
2025-11-16 22:06:41 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,31 @@
 from selenium.webdriver.common.by import By
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    CATALOGUE_LINK = (By.CSS_SELECTOR, ".dropdown-menu a")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_EMAIL = (By.NAME, "registration-email")
+    REGISTER_PASSWORD = (By.NAME, "registration-password1")
+    REGISTER_PASSWORD_REPEAT = (By.NAME, "registration-password2")
+    REGISTER_BUTTON = (By.NAME, "registration_submit")
+
+class ProductPageLocators():
+    PRODUCT_PAGE = (By.CSS_SELECTOR, ".image_container a")
+    BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main p")
+    PRODUCT_NAME_IN_THE_BASKET = (By.CSS_SELECTOR, ".alert-success strong")
+    PRODUCT_PRICE_IN_THE_BASKET = (By.CSS_SELECTOR, ".alert-info strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".fade")
+
+class BasketPageLocators():
+    EMPTY_BASKET_TEXT = (By.CSS_SELECTOR, "#content_inner p")
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
